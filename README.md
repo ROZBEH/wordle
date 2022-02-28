20cc2c37e13fd665106cdd086fa2666a09de6fd7
rozbeh <rouzbeh.asghari@gmail.com>
2022-02-28 07:59:08 -0800

    add readme

--- README.md
+++ README.md
@@ -5,5 +5,22 @@ A naive way to generate suggestions for the game of wordle
 
 In order to start the script simply run `python main.py` and follow the screen prompts.
 The script makes calls to [datamuse api](https://www.datamuse.com/api/). For example, if you
-pass `https://api.datamuse.com/words?sp=t??k` to the api, the api will return "*words that start 
+pass `https://api.datamuse.com/words?sp=t??k` to the api, the api will return "*all the words that start 
 with t, end in k, and have two letters in between*". 
+
+## Couple of Notes
+- The algorithm starts with a bunch of precusrsor suggestions. I just picked whatever word that came to my mind. One doesn't have to follow them. You can simply put whatever word you see as better fit. 
+- Once you make a suggestion, you then have to give a feedback to the script on how well the suggestions are by specifying something like `iicai`. 
+`c:correct letter, correct spot`
+`a:Correct letter, wrong spot`
+`i:Wrong letter`
+- After the feedback, the algorithm check which of the chars were correct and which ones are at the wrong place.
+Let's say the suggestion was `tesla` but the actual word is `their` so the feedback should be `caiia` which you 
+can infer based on the colors in the game. Once done, the algorithm then calls the api with the following query.
+`t????` which means that find all the 5 letter words that start with `t`. Among those, it then searches for the words
+that are allowed by checking which of the chars are among the forbidden chars. Please check the code for more details.
+- It is designed for the hard mode of wordle but it should work with easy mode too.
+- Although I tried it with a lot of wordle archive cases and it worked but it might fail
+at some of the edge cases.
+- The algorithm that I used might not be the most efficient ones in terms of big O. It has elements 
+of brute force in it.
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-A very simple python script to generate a set of suggestions for the game of wordle.
+A python script to generate a set of suggestions for the game of wordle.
 It assumes that you have access to the internet since it needs to call datamuse api.
 You can read more about the API here.
 # https://www.datamuse.com/api/
@@ -17,11 +17,12 @@ chars_none = []
 while True:
     print(all_responses[0:8])
     current_word = str(input('Please select one word from the above options.\nYou can also choose your own word of interest\n'))
+    current_word = current_word.lower()
     user_input = str(input('Enter the correctness of my prediction: \n options are c:correct, i:incorrect, a:almost correct\n'))
     if len(user_input) != 5:
         print('please enter valid options')
     else:
-        correct_idxes = [pos for pos, char in enumerate(user_input) if char == 'c']
+        correct_idxs = [pos for pos, char in enumerate(user_input) if char == 'c']
         almost_correct_idxs = [pos for pos, char in enumerate(user_input) if char == 'a']
         incorrect_idxs = [pos for pos, char in enumerate(user_input) if char == 'i']
         for item in almost_correct_idxs:
@@ -30,7 +31,7 @@ while True:
             chars_forbidden[item].append(current_word[item])
         almost_correct_chars = [current_word[i] for i in almost_correct_idxs]
         incorrect_chars = [current_word[i] for i in incorrect_idxs]
-        correct_chars = [current_word[i] for i in correct_idxes]
+        correct_chars = [current_word[i] for i in correct_idxs]
         intersection_chars_1 = list(set(correct_chars).intersection(incorrect_chars))
         intersection_chars_2 = list(set(almost_correct_chars).intersection(incorrect_chars))
         incorrect_chars = set(incorrect_chars) - set(intersection_chars_1) - set(intersection_chars_2)
@@ -40,7 +41,7 @@ while True:
         if len(incorrect_idxs) == 5:
             start_word_idx += 1
         # If some of the predictions are almost correct and all others are incorrect
-        elif len(correct_idxes) == 0 and len(almost_correct_idxs) > 0:
+        elif len(correct_idxs) == 0 and len(almost_correct_idxs) > 0:
             all_responses = []
             almost_correct_chars_joined = ''.join(almost_correct_chars)
             chars_with_unknowns = almost_correct_chars_joined + '?'*(5-len(almost_correct_chars_joined))
@@ -48,9 +49,8 @@ while True:
             str_permutation = [''.join(item) for item in str_permutation]
             str_permutation = list(set(str_permutation))
             for item in str_permutation:
-                response = requests.get("https://api.datamuse.com/words?sp=" + item + "&max=1000")
-                response_json = response.json()
-                for res in response_json:
+                response = requests.get("https://api.datamuse.com/words?sp=" + item + "&max=1000").json()
+                for res in response:
                     if res['word'] not in all_responses:
                         incorrect_intersection = list(set(list(res['word'])).intersection(chars_none))
                         almost_correct_intersection = list(set(list(res['word'])).intersection(almost_correct_chars))
@@ -65,12 +65,11 @@ while True:
         else:
             all_responses = []
             query_item = ['?']*5
-            for item in correct_idxes:
+            for item in correct_idxs:
                 query_item[item] = current_word[item]
             query_item = ''.join(query_item)
-            response = requests.get("https://api.datamuse.com/words?sp=" + query_item + "&max=1000")
-            response_json = response.json()
-            for res in response_json:
+            response = requests.get("https://api.datamuse.com/words?sp=" + query_item + "&max=1000").json()
+            for res in response:
                 if res['word'] not in all_responses:
                     incorrect_intersection = list(set(list(res['word'])).intersection(chars_none))
                     almost_correct_intersection = list(set(list(res['word'])).intersection(almost_correct_chars))
